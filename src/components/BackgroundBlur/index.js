2a42d1bafc6a836f1015205e836748b41b51d462
Jaagrav <jaagravseal03@gmail.com>
2021-04-05 00:53:04 +0530

    Fixed: Bubbles in backdrop

--- src/components/BackgroundBlur/index.js
+++ src/components/BackgroundBlur/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import style from "./style";
 
 //Importing Material UI Stuff
@@ -6,47 +6,25 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(style);
 
-function BlurredCircle({ index }) {
-  const classes = useStyles();
-  return (
-    <div
-      className={classes.blurredCircle}
-      style={{
-        height: window.innerWidth / 6,
-        width: window.innerWidth / 6,
-        top: (window.innerWidth / 6) * index * 2,
-        left: index % 2 === 0 ? "0" : window.innerWidth,
-        transform: index % 2 !== 0 ? "translateX(-100%)" : "",
-      }}
-    ></div>
-  );
-}
-
 function BackgroundBlur() {
   const classes = useStyles();
 
-  const [noOfCircles, setNoOfCircles] = useState([]);
-
-  useEffect(() => {
-    setNoOfCircles(
-      new Array(
-        Math.floor(
-          document.documentElement.scrollHeight / (window.innerWidth / 6)
-        )
-      ).fill("circle")
-    );
-  }, []);
-
   return (
-    <div
-      className={classes.backgroundBlur}
-      style={{
-        height: document.documentElement.scrollHeight,
-      }}
-    >
-      {noOfCircles.map((item, index) => (
-        <BlurredCircle key={`blurred-circle-${index}`} index={index} />
-      ))}
+    <div className={classes.backgroundBlur}>
+      <div
+        className={classes.blurredCircle1}
+        style={{
+          height: window.innerWidth / 6,
+          width: window.innerWidth / 6,
+        }}
+      ></div>
+      <div
+        className={classes.blurredCircle2}
+        style={{
+          height: window.innerWidth / 6,
+          width: window.innerWidth / 6,
+        }}
+      ></div>
     </div>
   );
 }
--- src/components/BackgroundBlur/style.js
+++ src/components/BackgroundBlur/style.js
@@ -2,17 +2,28 @@ export default function style(theme) {
   return {
     backgroundBlur: {
       zIndex: -1,
-      position: "absolute",
+      position: "fixed",
+      height: "100%",
       width: "100%",
       top: 0,
       left: 0,
       overflow: "hidden",
     },
-    blurredCircle: {
+    blurredCircle1: {
       borderRadius: 9999,
       backgroundColor: theme.palette.primary.blurColor,
       position: "absolute",
       filter: "blur(190px)",
+      top: 0,
+      left: 0,
+    },
+    blurredCircle2: {
+      borderRadius: 9999,
+      backgroundColor: theme.palette.primary.blurColor,
+      position: "absolute",
+      filter: "blur(190px)",
+      bottom: 0,
+      right: 0,
     },
   };
 }
