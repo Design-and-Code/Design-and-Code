339832f258f83d1bd1168d46acf8edd04783b1e5
Sidharth Rathi <Xodedeveloper@gmail.com>
2022-08-21 01:01:18 +0530

    resolving conflict

--- app/src/components/contact/sections/Contactus.tsx
+++ app/src/components/contact/sections/Contactus.tsx
@@ -1,31 +1,26 @@
-import { Grid, Typography } from '@mui/material';
-import { ContactusWrapper } from '../styles';
+import { Typography } from "@mui/material"
 
-const Contactus = () => {
+const ContactUs = () => {
   return (
-    <ContactusWrapper>
-      <Grid container className="section heroSection ">
-        <div className="heroSectionContent">
-          <Typography variant="h2" gutterBottom className="title">
-            Contact Us
-          </Typography>
-          <Typography variant="h6" gutterBottom className="description">
-            If you have any questions, we would love to hear from you. Feel free
-            to write us at{' '}
-            <a
-              href="mailto:designandcode.community@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-              className="link"
-            >
-              designandcode.community@gmail.com
-            </a>{' '}
-            and we{"'"}ll get back to you
-          </Typography>
-        </div>
-      </Grid>
-    </ContactusWrapper>
-  );
-};
+    <>
+      <Typography variant="h2" gutterBottom className={'title'}>
+        Contact Us
+      </Typography>
+      <Typography variant="h6" gutterBottom className={'description'}>
+        If you have any questions, we would love to hear from you. Feel free
+        to write us at{" "}
+        <a
+          href="mailto:designandcode.community@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className={'link'}
+        >
+          designandcode.community@gmail.com
+        </a>{" "}
+        and we'll get back to you
+      </Typography>
+    </>
+  )
+}
 
-export default Contactus;
+export default ContactUs;
