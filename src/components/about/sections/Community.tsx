7cb898ccb0d1e979a992579aa88181b9fb1125b5
Ayush Chugh <ayushchugh2006@gmail.com>
2022-09-24 01:28:28 +0530

    feat: :sparkles: lazy loading community image in about us page (#66)
    * feat: :sparkles: lazy loading community image in about us page
    
    * refactor: :recycle: next's image placeholder will be used for blur effect
    
    * refactor: :recycle: removed `loading` prop from `Image` component

--- src/components/about/sections/Community.tsx
+++ src/components/about/sections/Community.tsx
@@ -1,39 +1,52 @@
-import { Grid, Typography } from '@mui/material';
-import { CommunitySectionWrapper } from '../styles'
+import { Grid, Typography } from "@mui/material";
+import Image from "next/image";
+import { useState } from "react";
+import { CommunitySectionWrapper } from "../styles";
+import aboutIllustration from "~/assets/aboutIllustration.svg";
 
 const CommunitySection = () => {
-  return (
-    <CommunitySectionWrapper container>
-      <Grid container>
-        <Typography variant="h2" gutterBottom className={'title'}>
-          Our Community
-        </Typography>
-        <Grid container item xs={12} sm={6} className={'communityGrid'}>
-          <div className={'communityGrid1'}>
-            <Typography variant="h6" gutterBottom className='secondaryDesc'>
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </Typography>
-            <Typography variant="h6" gutterBottom className={'secondaryDesc'}>
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </Typography>
-          </div>
-        </Grid>
-        <Grid container item xs={12} sm={6}>
-          <div className={'communityGrid2'}>
-            <img
-              src={'/static/illustrations/aboutIllustration.svg'}
-              className={'cgimage'}
-              alt="Hero"
-            />
-          </div>
-        </Grid>
-      </Grid>
-    </CommunitySectionWrapper>
-  )
-}
+	return (
+		<CommunitySectionWrapper container>
+			<Grid container>
+				<Typography variant="h2" gutterBottom className={"title"}>
+					Our Community
+				</Typography>
+				<Grid container item xs={12} sm={6} className={"communityGrid"}>
+					<div className={"communityGrid1"}>
+						<Typography variant="h6" gutterBottom className="secondaryDesc">
+							Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+							amet sint. Velit officia consequat duis enim velit mollit.
+							Exercitation veniam consequat sunt nostrud amet.
+						</Typography>
+						<Typography variant="h6" gutterBottom className={"secondaryDesc"}>
+							Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+							amet sint. Velit officia consequat duis enim velit mollit.
+							Exercitation veniam consequat sunt nostrud amet.
+						</Typography>
+					</div>
+				</Grid>
+				<Grid container item xs={12} sm={6}>
+					<div className={"communityGrid2"}>
+						<Image
+							src={aboutIllustration}
+							placeholder="blur"
+							blurDataURL="../../../assets/aboutIllustrationPlaceholder.png"
+							height={400}
+							width={400}
+							alt="community illustration"
+						/>
+						{/* <img
+							id="img"
+							src={"/static/illustrations/aboutIllustrationStatic.svg"}
+							onLoad={e => console.log("Loaded")}
+							className={"cgimage"}
+							alt="Hero"
+						/> */}
+					</div>
+				</Grid>
+			</Grid>
+		</CommunitySectionWrapper>
+	);
+};
 
 export default CommunitySection;
