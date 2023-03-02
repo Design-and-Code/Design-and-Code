d15a576d443cb9df4457ade5d3aa5b5fb4cedd2f
Alkaison <505ganeshmourya@gmail.com>
2023-03-02 15:22:09 +0530

    fix: not found error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,52 @@ import createEmotionServer from '@emotion/server/create-instance';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { createEmotionCache, darkTheme as theme } from '~/lib/theme';
 
+/*   
+  the title variable will be set to the text content of the 
+  title tag if it exists, or to a default value of "Design and Code" if it doesn't.
+*/  
+const { head } = initialProps;
+const titleElement = head.find((h) => h.type === 'title');
+const title = titleElement ? titleElement.props.children : 'Design and Code';
+
+/* get page title and set to meta content */
+const metaTitle = document.title;
+
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage;
+
+    // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
+    // However, be aware that it can have global side effects.
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          <App emotionCache={cache} {...props} />
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    // This is important. It prevents Emotion to render invalid HTML.
+    // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        key={style.key}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
+
+    return {
+      ...initialProps,
+      emotionStyleTags
+    };
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -64,51 +109,5 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
-  }
-}
-
-MyDocument.getInitialProps = async (ctx) => {
-  const originalRenderPage = ctx.renderPage;
-
-  // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
-  // However, be aware that it can have global side effects.
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App: any) =>
-        function EnhanceApp(props) {
-          return <App emotionCache={cache} {...props} />;
-        },
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-  // This is important. It prevents Emotion to render invalid HTML.
-  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
-  const emotionStyles = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
-      key={style.key}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ));
-
-/*   
-  the title variable will be set to the text content of the 
-  title tag if it exists, or to a default value of "Design and Code" if it doesn't.
-*/  
-  const { head } = initialProps;
-  const titleElement = head.find((h) => h.type === 'title');
-  const title = titleElement ? titleElement.props.children : 'Design and Code';
-
-  /* get page title and set to meta content */
-  const metaTitle = document.title;
-
-  return {
-    ...initialProps,
-    emotionStyleTags,
   };
 };
